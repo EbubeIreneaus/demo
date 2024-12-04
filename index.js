ffd899afa785c896e8a6c6c3cff0cceac9b3f7bb
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-12-04 11:28:11 +0100

    lacation added as person attributes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const person = {
     name: "Okigwe Ebube Ireneaus",
     from: "Enugu, Nigeria",
-    job: 'Software developer'
+    job: 'Software developer',
+    location: "Abuja"
 }
 
 console.log(person.name);
